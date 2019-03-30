f09f54073dc698564bdd2330d9cc03d9cd02a2cc
Andrew Liebchen <andrewliebchen@gmail.com>
2019-03-29 22:15:27 -0700

    Get basic functionality working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import useInterval from "@use-hooks/interval";
-import { Flex, Text, Button, Box } from "rebass";
+import { Flex, Text } from "rebass";
 import randomFlatColors from "random-flat-colors";
 import hello from "hello-color";
 import styled from "styled-components";
 import Title from "react-simple-title";
+import padStart from "lodash.padstart";
 
-const pomoLength = 100;
+const pomoLength = 10;
 const color = hello(randomFlatColors());
+
+const Countdown = styled(Text)`
+  font-feature-settings: "tnum";
+`;
 
 const App = props => {
   let [active, setActive] = useState(false);
@@ -22,6 +27,14 @@ const App = props => {
     }
   }, 1000);
 
+  const minutes = Math.floor(count / 60);
+  const seconds = Math.round((count / 60 - minutes) * 60);
+  const formattedCountdown = `${padStart(minutes, 2, "0")}:${padStart(
+    seconds,
+    2,
+    "0"
+  )}`;
+
   return (
     <Flex
       width="100vw"
@@ -29,21 +42,25 @@ const App = props => {
       alignItems="center"
       flexDirection="column"
       bg={color.base}
+      onClick={() => setActive(!active)}
       style={{
         height: "100vh"
       }}
     >
-      <Title>{count}</Title>
-      <Text color={color.color} fontSize={6} fontWeight="bold">
-        {count}
-      </Text>
-      <Button
-        onClick={() => setActive(!active)}
-        color={color.base}
-        bg={color.color}
-      >
-        {active ? "Stop" : "Go"}
-      </Button>
+      <Title>{formattedCountdown}</Title>
+      <Countdown color={color.color} fontSize={6} fontWeight="bold">
+        {formattedCountdown}
+      </Countdown>
+      <div style={{ width: "100vw", height: 20, position: "fixed", top: 0 }}>
+        <div
+          style={{
+            width: `${((pomoLength - count) / pomoLength) * 100}vw`,
+            height: "inherit",
+            backgroundColor: color.color,
+            transition: "0.9s ease-in-out"
+          }}
+        />
+      </div>
     </Flex>
   );
 };
