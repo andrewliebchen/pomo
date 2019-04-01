54a1671b7826be439093ca6d4fa7a7ff1a682491
Andrew Liebchen <andrewliebchen@gmail.com>
2019-03-31 22:08:15 -0700

    Add background color on progress

--- src/Progress.js
+++ src/Progress.js
@@ -1,21 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import hexOpacity from "hex-opacity";
 
 const Root = styled.div`
   width: 100vw;
   height: 20px;
   position: fixed;
   top: 0;
+  background-color: ${props => hexOpacity.create(props.color, 0.1)};
 `;
 
 const Fill = styled.div`
   height: inherit;
   background-color: ${props => props.color};
-  transition: 1s linear;
 `;
 
 const Progress = props => (
-  <Root>
+  <Root {...props}>
     <Fill
       style={{
         width: `${((props.length - props.count) / props.length) * 100}vw`
